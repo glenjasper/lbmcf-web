689953dfcf524839d95d18957cdea9eb8d7f258b
Glen Jasper <glen.jasper7@gmail.com>
2020-11-08 16:16:42 -0300

    Update

--- apps/research/models.py
+++ apps/research/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     description = models.TextField(blank = True, verbose_name = 'Description', help_text = 'Project summary')
     financer = models.ForeignKey(to = Financer, on_delete = models.CASCADE, verbose_name = 'Financer name')
     date = models.DateField(help_text = 'project start date', verbose_name = 'Start date')
-    image = models.ImageField(upload_to = "projects", verbose_name = "Reference image")
+    image = models.ImageField(upload_to = 'projects', verbose_name = "Reference image")
     status = models.BooleanField(default = True, verbose_name =  'Status')
     created = models.DateTimeField(auto_now_add = True, blank = True, null = True, verbose_name = 'Creation date')
     updated = models.DateTimeField(auto_now = True, blank = True, null = True, verbose_name = 'Modification date')
