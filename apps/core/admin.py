0979412cfbba42c947fb4aac08baa280cd085e15
Glen Jasper <glen.jasper7@gmail.com>
2020-11-21 14:29:17 -0300

    Update

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -14,6 +14,14 @@ from .models import (
     PatentType
 )
 
+class FinancerAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'full_name',
+        'status',
+    ]
+    readonly_fields = ['created', 'updated']
+
 class AcademicDegreeAdmin(admin.ModelAdmin):
     list_display = [
         'name',
@@ -23,7 +31,15 @@ class AcademicDegreeAdmin(admin.ModelAdmin):
     ]
     readonly_fields = ['created', 'updated']
 
-class FinancerAdmin(admin.ModelAdmin):
+class RoleAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'order',
+        'status',
+    ]
+    readonly_fields = ['created', 'updated']
+
+class DepartmentAdmin(admin.ModelAdmin):
     list_display = [
         'name',
         'full_name',
@@ -62,22 +78,6 @@ class InstitutionAdmin(admin.ModelAdmin):
     ordering = ['full_name']
     search_fields = ['name', 'full_name', 'country__name', 'state__name']
     # list_filter = ['country__name, state__name']
-
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display = [
-        'name',
-        'full_name',
-        'status',
-    ]
-    readonly_fields = ['created', 'updated']
-
-class RoleAdmin(admin.ModelAdmin):
-    list_display = [
-        'name',
-        'order',
-        'status',
-    ]
-    readonly_fields = ['created', 'updated']
 
 class GraduateProgramAdmin(admin.ModelAdmin):
     list_display = [
@@ -118,13 +118,13 @@ class PatentTypeAdmin(admin.ModelAdmin):
     ]
     readonly_fields = ['created', 'updated']
 
-admin.site.register(AcademicDegree, AcademicDegreeAdmin)
 admin.site.register(Financer, FinancerAdmin)
+admin.site.register(AcademicDegree, AcademicDegreeAdmin)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(Department, DepartmentAdmin)
 admin.site.register(Country, CountryAdmin)
 admin.site.register(State, StateAdmin)
 admin.site.register(Institution, InstitutionAdmin)
-admin.site.register(Department, DepartmentAdmin)
-admin.site.register(Role, RoleAdmin)
 admin.site.register(GraduateProgram, GraduateProgramAdmin)
 admin.site.register(ProtocolType, ProtocolTypeAdmin)
 admin.site.register(FileType, FileTypeAdmin)
